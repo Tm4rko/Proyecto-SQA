6165337fe589a8694fbd3fd51b9d34a7bf6283fa
SergioLara04 <lrs2023984@est.univalle.edu>
2024-11-27 13:30:01 -0400

    WEB locators
    Aquí añado los web locators pero para cargar imagen se debe cambiar la ruta para que coincida con la computadora para cargar las imagenes

---
+++ SQAFinal/test_cases/test_Sucursal/test_Sucursal.py
@@ -0,0 +1,19 @@
+# Incluir acá los test cases para el módulo específico.
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+# Configuración automática del WebDriver
+driver = webdriver.Chrome()
+
+# Maximizar la ventana del navegador
+driver.maximize_window()
+
+# Abrir url en el navegador
+driver.get('')
+time.sleep(2)
+
+
+
+driver.quit()
